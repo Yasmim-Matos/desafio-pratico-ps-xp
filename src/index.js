f8699ff3cf449e04e895e4e0cc381eb2f26e6017
Yasmim-Matos <yasmimmn123@gmail.com>
2022-07-15 21:41:04 -0300

    feat: installing react-router-dom v5 and fixing 'App.js' and 'App.css' files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
