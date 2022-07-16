29f3e3298dc5b1092f61ad0f857b2925018f7400
Yasmim-Matos <yasmimmn123@gmail.com>
2022-07-16 20:36:55 -0300

    fix: fixing the route for '/stocklist'

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
-import DepositAndWithdrawalPage from './pages/DepositAndWithdrawalPage';
+import StockListPage from './pages/StockListPage';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/depositAndWithdrawal" component={ DepositAndWithdrawalPage } />
+        <Route path="/stocklist" component={ StockListPage } />
         <Route exact path="/" component={ LoginPage } />
       </Switch>
     </div>
