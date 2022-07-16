e807b39abda317a41aee923bc5b32a947b3b2b47
Yasmim-Matos <yasmimmn123@gmail.com>
2022-07-15 22:07:34 -0300

    feat: creating 'DepositAndWithdrawalPage'

---
+++ src/pages/DepositAndWithdrawalPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DepositAndWithdrawal() {
+    return (
+        <main className="depositAndWithdrawal-container">
+            <h1>Deposit And Withdrawal</h1>
+        </main>
+    );
+}
+
+export default DepositAndWithdrawal;
