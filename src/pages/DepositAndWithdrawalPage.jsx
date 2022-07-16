007e82b5bae7cf861538ce498172e2609b8b001a
Yasmim-Matos <yasmimmn123@gmail.com>
2022-07-16 20:45:52 -0300

    feat: creating the correct 'DepositAndWithdrawalPage'

---
+++ src/pages/DepositAndWithdrawalPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function DepositAndWithdrawalPage() {
+    return (
+        <main className="deposit-withdrawal-container">
+            <Header />
+            <h1>Deposit And Withdrawal</h1>
+        </main>
+    );
+}
+
+export default DepositAndWithdrawalPage;
