b8bdf1fe59863e773d31bb925bb0159f48b4d0bf
Yasmim-Matos <yasmimmn123@gmail.com>
2022-07-21 04:42:30 -0300

    feat: creating 'UserStocks' component

---
+++ src/components/UserStocks.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function UserStocks() {
+    return (
+        <main className="header-container">
+            <h3>Minhas ações</h3>
+                <table>
+                    <thead>
+                        <tr>
+                        <th>Nome da Ação</th>
+                        <th>Quantidade</th>
+                        <th>Valor Pago (R$)</th>
+                        <th>Negociar</th>
+                        </tr>
+                    </thead>
+                </table>
+        </main>
+    );
+}
+
+export default UserStocks;
