599a59af37a41fb512038278c7692ab55d230793
Yasmim-Matos <yasmimmn123@gmail.com>
2022-07-21 03:17:52 -0300

    feat: adding 'AppProvider' function on 'App.js' file to provide the informations to the routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import AppProvider from './context/AppProvider';
 
 import LoginPage from './pages/LoginPage';
 import StockListPage from './pages/StockListPage';
@@ -10,14 +11,14 @@ import DepositAndWithdrawalPage from './pages/DepositAndWithdrawalPage';
 
 function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={ LoginPage } />
-        <Route path="/stocklist" component={ StockListPage } />
-        <Route path="/buyandsell" component={ BuyAndSellPage } />
-        <Route path="/depositandwithdrawal" component={ DepositAndWithdrawalPage } />
-      </Switch>
-    </div>
+      <AppProvider>
+        <Switch>
+          <Route exact path="/" component={ LoginPage } />
+          <Route path="/stocklist" component={ StockListPage } />
+          <Route path="/buyandsell" component={ BuyAndSellPage } />
+          <Route path="/depositandwithdrawal" component={ DepositAndWithdrawalPage } />
+        </Switch>
+      </AppProvider>
   );
 }
 
