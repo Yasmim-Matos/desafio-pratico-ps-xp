dd6eea8957c7a1921009bb1716285991fc6c4742
Yasmim-Matos <yasmimmn123@gmail.com>
2022-07-21 18:50:53 -0300

    feat: adding 'StocksAvailableData' file to populate the 'StocksAvailableToInvestTable' with data

---
+++ src/utils/StocksAvailableData.js
@@ -0,0 +1,10 @@
+const StocksAvailableData = [
+    {
+        stockName: 'XP1',
+        qtdAvailable: 1,
+        unitValue: 10,
+        lotValue: 10,
+    },
+];
+
+export default StocksAvailableData;
