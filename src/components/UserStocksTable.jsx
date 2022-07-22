d11ec062bee1e4a704ed52a9f660a68ed718ff05
Yasmim-Matos <yasmimmn123@gmail.com>
2022-07-22 19:05:27 -0300

    fix: importing data from 'UserStocksData' file into 'UserStocks' file

--- src/components/UserStocksTable.jsx
+++ src/components/UserStocksTable.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import StocksBoughtData from '../utils/StocksBoughtData';
+import UserStocksData from '../utils/UserStocksData';
 
 function UserStocks() {
     return (
         <main className="user-stocks-container">
             <h3>Minhas ações</h3>
 
-            <StocksBoughtData />
+            <UserStocksData />
         </main>
     );
 }
