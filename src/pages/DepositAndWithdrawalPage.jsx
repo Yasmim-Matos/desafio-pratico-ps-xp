b75e6f26d783a489560272fbb55adf19034e48c7
Yasmim-Matos <yasmimmn123@gmail.com>
2022-07-23 02:33:57 -0300

    feat: 'userBalance' and buttons: 'Deposito' and 'Retirada' were added in 'DepositAndWithdrawalPage'

--- src/pages/DepositAndWithdrawalPage.jsx
+++ src/pages/DepositAndWithdrawalPage.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 import Header from '../components/Header';
 
 function DepositAndWithdrawalPage() {
+    const {
+        userData: {
+            userBalance,
+        },
+    } = useContext(AppContext);
+
     return (
         <main className="deposit-withdrawal-container">
             <Header />
             <h1>Deposit And Withdrawal</h1>
+
+            <p>
+                Saldo:
+                {' '}
+                <span className="balance">{ userBalance }</span>
+            </p>
+
+            <input type="number" />
+
+
+            <button
+                type="button"
+            >
+                Depósito
+            </button>
+
+            <button
+                type="button"
+            >
+                Retirada
+            </button>
         </main>
     );
 }
