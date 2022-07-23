953876ee6351fc120eab1373f5594cab43028deb
Yasmim-Matos <yasmimmn123@gmail.com>
2022-07-22 21:46:49 -0300

    fix: the return of the table that was in the file 'AvailableStocksToInvestTable' was changed to the file 'AvailableStocksToInvestData'

--- src/utils/AvailableStocksToInvestData.js
+++ src/utils/AvailableStocksToInvestData.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function AvailableStocksToInvestData() {
     const availableStocksToInvest = [
         {
@@ -13,6 +15,53 @@ function AvailableStocksToInvestData() {
             lotValue: 100,
         },
     ];
+
+    return (
+        <>
+            { availableStocksToInvest.length === 0
+                    ? <p>Não existe nenhuma ação disponível para investir no momento</p> 
+                    :
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Nome da Ação</th>
+                                <th>Quantidade</th>
+                                <th>Valor Unitário (R$)</th>
+                                <th>Valor do Lote (R$)</th>
+                                <th>Negociar</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                                { availableStocksToInvest.map(({ stockName, qtdAvailable, unitValue, lotValue}) => {
+                                    return (
+                                        <tr key={ stockName }>
+                                            <td>{ stockName }</td>
+                                            <td>{ qtdAvailable }</td>
+                                            <td>{ unitValue }</td>
+                                            <td>{ lotValue }</td>
+                                            <td>
+                                                <button
+                                                type="button"
+                                                name="buy-button"
+                                                >
+                                                C
+                                                </button>
+                                                <button
+                                                type="button"
+                                                name="sell-button"
+                                                >
+                                                V
+                                                </button>
+                                            </td>
+                                        </tr>
+                                        );
+                                    })}
+                        </tbody>
+                    </table>
+            }
+        </>
+    );
 }
 
 export default AvailableStocksToInvestData;
