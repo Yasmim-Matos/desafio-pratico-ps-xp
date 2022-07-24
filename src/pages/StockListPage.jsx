5e1ee35dbd57eb524fde527231b63dbb0c1fc806
Yasmim-Matos <yasmimmn123@gmail.com>
2022-07-23 23:51:43 -0300

    feat (style): the basic style for 'StockListPage' was added on 'StockListPage' component

--- src/pages/StockListPage.jsx
+++ src/pages/StockListPage.jsx
@@ -5,33 +5,38 @@ import { Link } from 'react-router-dom';
 import UserStocksTable from '../components/UserStocksTable';
 import AvailableStocksToInvestTable from '../components/AvailableStocksToInvestTable';
 
+import '../styles/StockListPage.css';
+
 function StockListPage() {
     return (
-        <main className="stock-list-container">
+        <main>
             <Header />
-            <h1>Stock List</h1>
 
-            <div className="user-stocks-container">
-                <h3>Minhas ações</h3>
+            <section className="stock-list-container">
+                <h1>Stock List</h1>
 
-                <UserStocksTable />
-            </div>
+                <div className="user-stocks-container">
+                    <h3>Minhas ações</h3>
 
-            <div className="stocks-available-table-container">
-                <h3>Disponíveis para investir</h3>
+                    <UserStocksTable />
+                </div>
 
-                <AvailableStocksToInvestTable />
-            </div>
+                <div className="stocks-available-table-container">
+                    <h3>Disponíveis para investir</h3>
+
+                    <AvailableStocksToInvestTable />
+                </div>
 
 
-            <Link to='/depositandwithdrawal'>
-                <button
-                    type="button"
-                    name="stock-list-button"
-                >
-                    Depósito/Retirada
-                </button>
-            </Link>
+                <Link to='/depositandwithdrawal'>
+                    <button
+                        type="button"
+                        name="stock-list-button"
+                    >
+                        Depósito/Retirada
+                    </button>
+                </Link>
+            </section>
         </main>
     );
 }
